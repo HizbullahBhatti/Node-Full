8da8ad6b3831d6b5f6cc689701a346e331bffe8b
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-01-10 17:35:18 +0500

    file created using fs module

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+
+fs.writeFileSync('hello.txt', 'Hello from Node.js');
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
