04531e5aed67e687f1c2ae951a71e25a810001a6
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-06-20 14:44:56 +0500

    commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        id: 1,
+        name: 'John Doe',
+        age: 22,
+    },
+    {
+        id: 2,
+        name: 'Jane Carry',
+        age: 23,
+    },
+    {
+        id: 3,
+        name: 'John Smith',
+        age: 24,
+    }
+]
+
+
+module.exports = data
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+const path = require('path');
+
+
+
+// app.get('/', (req, res) => {
+//     console.log(req.query.name);
+//     res.send('Welcome this is Home Page of '+`<a href="/about">About</a> <a href="/help">Help</a>`) 
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send([{name:'Hizbullah', age: 22}])
+// })
+
+// app.get('/help', (req, res) => {
+//     res.send(`<input type="text" placeholder="Enter your name" /> <button>Submit</button>`) 
+// })
+
+app.set('view engine', 'ejs');
+
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+const PORT = process.env.PORT || 4550;
+
+app.get('/profile', (req, res) => {
+    const user={
+        name: 'Hizbullah',
+        age: 22,
+        hobbies: ['Coding', 'Reading', 'Gaming']
+    }
+    res.render('profile',{user})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+
+
+
+
+app.listen(4550, () => console.log(`Server is running on port ${PORT}`))
